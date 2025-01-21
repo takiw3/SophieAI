a0da381d6782263ef0a15abbe988cc18c6b8484c
takiw3 <123529543+takiw3@users.noreply.github.com>
2025-01-20 19:08:09 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: '#13001f',
+          100: '#1a0029',
+          200: '#20003d'
+        },
+        purple: {
+          glow: '#8b5cf6'
+        }
+      },
+      boxShadow: {
+        'purple-glow': '0 0 20px rgba(139, 92, 246, 0.3)',
+        'purple-glow-lg': '0 0 30px rgba(139, 92, 246, 0.4)'
+      }
+    },
   },
   plugins: [],
 };
